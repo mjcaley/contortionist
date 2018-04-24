86400559649ad232c90c5b1694a74e850ee5c7cb
Michael Caley <mjcaley@darkarctic.com>
2018-04-23 22:06:46 -0400

    Deleted delete_me

--- setup.py
+++ setup.py
@@ -36,5 +36,11 @@ setup(
 
     python_requires='!=2.*,!=3.0,!=3.1,!=3.2,!=3.3,!=3.4',
     setup_requires=['pytest-runner', ],
+    install_requires=['toml',
+                      'aiofiles',
+                      'aiosmtpd',
+                      'aiosmtplib',
+                      'aiosqlite',
+                      'aiospamc'],
     tests_require=['pytest-asyncio>=0.6', 'pytest-cov', 'pytest>=3.0', 'asynctest'],
 )
