0992eba6d767e5b188fd4af21dc0a2c6f93e41af
Michael Caley <mcaley@canadagoose.com>
2018-04-24 16:06:10 -0400

    Added entry point, might scrap it later

---
+++ contortionist/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+class Application:
+    def get_environment(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = Application()
+    app.run()
